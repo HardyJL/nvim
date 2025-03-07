780f5709f088a0944a4a3d38968fbe37ffb5cec2
Johannes Hardenberg <33319906+HardyJL@users.noreply.github.com>
2025-03-07 10:16:37 +0100

    change remaps

--- lua/theprimeagen/set.lua
+++ lua/theprimeagen/set.lua
@@ -1,5 +1,3 @@
-vim.opt.guicursor = ""
-
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
@@ -23,9 +21,4 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-
-vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = "80"
+vim.opt.updatetime = 100
